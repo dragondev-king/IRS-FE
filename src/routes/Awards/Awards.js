d7c16537c1e6fc94cf65e534f04fad39792edd1d
Kenji Nakamura <dragondev1017@gmail.com>
2022-02-11 22:29:43 +0900

    Create basic routes that are integrated with API

--- src/routes/Awards/Awards.js
+++ src/routes/Awards/Awards.js
@@ -1,6 +1,28 @@
+import { useState, useEffect } from 'react'
+
+import { getData } from '../../api/call'
+
 const Awards = () => {
+  const [loading, setLoading] = useState(true)
+  const [awards, setAwards] = useState([])
+  
+  useEffect(() => {
+    setLoading(true)
+    getData({url: '/awards'})
+    .then((data) => {
+      setAwards(data)
+      setLoading(false)
+    })
+    .catch((err) => console.error(err))
+  }, [setAwards])
   return (
-    <>Awards</>
+    <>
+      {loading && <div>Loading ...</div>}
+      { !loading &&awards.map((award, idx) => {
+
+        return <p key={idx}>{award.amount} {award.purpose} {award.taxperiod} {award.filing_id}</p>
+      })}
+    </>
   )
 }
 
--- src/routes/Filings/Filings.js
+++ src/routes/Filings/Filings.js
@@ -1,6 +1,28 @@
+import { useState, useEffect } from 'react'
+
+import { getData } from '../../api/call'
+
 const Filings = () => {
+  const [loading, setLoading] = useState(true)
+  const [filings, setFilings] = useState([])
+  
+  useEffect(() => {
+    setLoading(true)
+    getData({url: '/filings'})
+    .then((data) => {
+      setFilings(data)
+      setLoading(false)
+    })
+    .catch((err) => console.error(err))
+  }, [setFilings])
   return (
-    <>Filings</>
+    <>
+      {loading && <div>Loading ...</div>}
+      { !loading &&filings.map((filing, idx) => {
+
+        return <p key={idx}>{filing.filer_id} {filing.filer_name} {filing.recipient_id} {filing.recipient_name}</p>
+      })}
+    </>
   )
 }
 
