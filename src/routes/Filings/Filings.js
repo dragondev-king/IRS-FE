014b1b654c009343ab40f57debb6feafaaa8c5b4
dragondev-king <dragondev1017@gmail.com>
2022-02-12 03:19:29 +0900

    WIP: Create Filter component

--- src/routes/Filings/Filings.js
+++ src/routes/Filings/Filings.js
@@ -1,13 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
 import DataTable from '../../components/DataTable'
 import SideBar from '../../components/SideBar'
+import Filter from '../../components/Filter'
 import { getData } from '../../api/call'
 import { sideBarItems, filingFields } from '../../constants'
 
 const Filings = () => {
   const [loading, setLoading] = useState(true)
   const [filings, setFilings] = useState([])
+  const [filers, setFilers] = useState([])
   
   useEffect(() => {
     setLoading(true)
@@ -18,11 +20,30 @@ const Filings = () => {
     })
     .catch((err) => console.error(err))
   }, [setFilings])
+
+  useEffect(() => {
+    getData({url: '/filers'})
+    .then((data) => {
+      setFilers(data)
+    })
+    .catch((err) => console.error(err))
+  }, [setFilers])
+
+  const filerOptions = useMemo(() =>[
+    {label: 'All', value: ''},
+    ...filers ? filers.map((filer) => ({
+      value: filer.id,
+      label: filer.name
+    })) : []
+  ],[filers]
+  )
+
   return (
     <div className="container">
       <div className="row"> 
         <div className="col-3">
           <SideBar items={sideBarItems}/>
+          <Filter options={filerOptions} />
         </div>
         <div className="col-9">
           {loading && <div>Loading ...</div>}
