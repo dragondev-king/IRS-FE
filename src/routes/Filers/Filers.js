53a0da6fb07d99f8c8aa0b70d55ca3b02627bd8e
dragondev-king <dragondev1017@gmail.com>
2022-02-11 22:29:43 +0900

    Create basic routes that are integrated with API

--- src/routes/Filers/Filers.js
+++ src/routes/Filers/Filers.js
@@ -3,18 +3,21 @@ import { useEffect, useState } from "react"
 import { getData } from "../../api/call"
 
 const Filers = () => {
+  const [loading, setLoading] = useState(true)
   const [filers, setFilers] = useState([])
   
   useEffect(() => {
     getData({url: '/filers'})
     .then((data) => {
       setFilers(data)
+      setLoading(false)
     })
     .catch((err) => console.error(err))
   }, [setFilers])
   return (
     <>
-      {filers.map((filer, idx) => {
+      {loading && <div>Loading ...</div>}
+      {!loading && filers.map((filer, idx) => {
 
         return <p key={idx}>{filer.ein} {filer.name} {filer.addess} {filer.city} {filer.state} {filer.zipcode}</p>
       })}
--- src/routes/Recipients/Recipients.js
+++ src/routes/Recipients/Recipients.js
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react"
+
+import { getData } from "../../api/call"
+
 const Recipients = () => {
-  return <>Recipients</>
+  const [loading, setLoading] = useState(true)
+  const [recipients, setRecipients] = useState([])
+  
+  useEffect(() => {
+    setLoading(true)
+    getData({url: '/recipients'})
+    .then((data) => {
+      setRecipients(data)
+      setLoading(false)
+    })
+    .catch((err) => console.error(err))
+  }, [setRecipients])
+  return (
+    <>
+      {loading && <div>Loading ...</div>}
+      { !loading &&recipients.map((recipient, idx) => {
+
+        return <p key={idx}>{recipient.ein} {recipient.name} {recipient.addess} {recipient.city} {recipient.state} {recipient.zipcode}</p>
+      })}
+    </>
+  )
 }
 
 export default Recipients
