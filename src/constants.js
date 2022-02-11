29ffe137b978bfbfbe755d49c708cb6572baca74
dragondev-king <dragondev1017@gmail.com>
2022-02-12 01:39:52 +0900

    Create Sidebar and apply it

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const sideBarItems = ['filers', 'recipients', 'awards', 'filings']
+
+export const organizationFields = ['ein', 'name', 'address', 'city', 'state', 'zipcode']
+
+export const awardFields = ['amount', 'purpose', 'tax_period']
+
+export const filingFields = ['filer_name', 'recipient_name', 'purpose', 'amount', 'tax_period']
--- src/routes/Recipients/Recipients.js
+++ src/routes/Recipients/Recipients.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 
 import DataTable from "../../components/DataTable"
 import { getData } from "../../api/call"
+import SideBar from "../../components/SideBar"
+import { sideBarItems, organizationFields } from "../../constants"
 
 const Recipients = () => {
   const [loading, setLoading] = useState(true)
@@ -18,10 +20,17 @@ const Recipients = () => {
   }, [setRecipients])
   return (
     <div className="container">
-      {loading && <div>Loading ...</div>}
-      {!loading && 
-        <DataTable data={recipients} fieldNames={['ein', 'name', 'address', 'city', 'city', 'zipcode']} tableName='Recipients'/>
-      }
+      <div className="row"> 
+        <div className="col-3">
+          <SideBar items={sideBarItems}/>
+        </div>
+        <div className="col-9">
+          {loading && <div>Loading ...</div>}
+          {!loading && 
+            <DataTable data={recipients} fieldNames={organizationFields} tableName='Recipients'/>
+          }
+        </div>
+      </div>
     </div>
   )
 }
