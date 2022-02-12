39433dd2dfda6ff15708da68c6bcd543c989c724
dragondev-king <dragondev1017@gmail.com>
2022-02-13 01:58:33 +0900

    WIP: Implement filtering the recipients

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,14 +2,13 @@ import { useCallback, useState } from "react";
 import Select from "react-select"
 
 const Filter = ({ options, onFilterChange }) => {
-  const [selectedItem, setSelectedItem] = useState({})
+  const [selectedItem, setSelectedItem] = useState()
   const handleChange = useCallback((option) => {
     setSelectedItem(option)
     onFilterChange(option.value)
   }, [onFilterChange])
   return (
     <Select
-      defaultValue='All'
       value={selectedItem}
       onChange={handleChange}
       options={options}
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
-export const sideBarItems = ['filers', 'recipients', 'awards', 'filings']
+export const sideBarItems = ['filers', 'recipients', 'filings','awards']
 
 export const organizationFields = ['ein', 'name', 'address', 'city', 'state', 'zipcode']
 
 export const awardFields = ['amount', 'purpose', 'tax_period']
 
-export const filingFields = ['filer_name', 'recipient_name', 'purpose', 'amount', 'tax_period']
+export const filingFields = ['id', 'filer_name', 'recipient_name', 'purpose', 'amount', 'tax_period']
